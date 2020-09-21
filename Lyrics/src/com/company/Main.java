f1f65190b19a873ca3d68a23878b155baa1d040a
intexisty <30681593+intexisty@users.noreply.github.com>
2020-09-21 09:08:44 -0500

    create lyrics program

---
+++ Lyrics/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Never gonna give you up");
+	    System.out.println("Never gonna let you down");
+    }
+}
